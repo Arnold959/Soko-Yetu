c7c8ed4b1baae5bae42ba440e13cea63a89e1d9d
winniegithubrit <winnie.jomo@student.moringaschool.com>
2023-06-06 15:26:28 +0300

    Complete

--- Ecommerce.py
+++ Ecommerce.py
@@ -1,14 +1,14 @@
-from sqlalchemy import String, Column, Integer, create_engine, ForeignKey
+from sqlalchemy import String, Column, Integer, create_engine, ForeignKey,text
 from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     name = Column(String(50))
-    sales = relationship("Sales", backref="user")
-    reviews = relationship("Review", backref="user")
+    sales = relationship("Sales", backref="sales_user")
+    user_reviews = relationship("Review", backref="reviews_user")
 
 class Product(Base):
     __tablename__ = 'product'
@@ -19,37 +19,84 @@ class Product(Base):
     category_id = Column(Integer, ForeignKey("category.id"))
     stock = Column(Integer)
     image_url = Column(String)
-    reviews = relationship("Review", backref="product")
-    sales = relationship("Sales", backref="product")
+    reviews = relationship("Review", backref="reviews_product")
+    sales = relationship("Sales", backref="salers_product")
+    category = relationship("Category", backref="categorys_products")
   
 class Review(Base):
     __tablename__ = 'review'
     id = Column(Integer, primary_key=True)
     product_id = Column(Integer, ForeignKey("product.id"))
-    user_id = Column(Integer, ForeignKey("user.id")) 
+    user_id = Column(Integer, ForeignKey("users.id"))
     rating = Column(Integer)
     comment = Column(String)
-    user = relationship("User", backref="reviews")
-    product = relationship("Product", backref="reviews")
+    user = relationship("User", backref="users_reviews")
+    product = relationship("Product", backref="products_reviews")
 
 class Category(Base):
     __tablename__ = 'category'
     id = Column(Integer, primary_key=True)
     name = Column(String(50))
-    products = relationship("Product", backref="category")
+    products = relationship("Product", backref="products_category")
 
 class Sales(Base):
     __tablename__ = 'sales'
     id = Column(Integer, primary_key=True)
     product_id = Column(Integer, ForeignKey("product.id"))
-    user_id = Column(Integer, ForeignKey("user.id")) 
+    user_id = Column(Integer, ForeignKey("users.id"))
     quantity = Column(Integer)
     price = Column(Integer)
-    sales_user = relationship("User", backref="sales")
-    product = relationship("Product", backref="sales")
+    user = relationship("User", backref="user_sales")
+    product = relationship("Product", backref="products_sales")
 
 engine = create_engine('sqlite:///database.db')
 Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
 session = Session()
+session.execute(text("DROP TABLE IF EXISTS user"))
+session.commit()
+
+# commodity1 = Product(
+#     id=1,
+#     name="Laptop",
+#     price=50000,
+#     description="A laptop",
+#     category_id=1,
+#     stock=3,
+#     image_url=""
+# )
+
+# review1 = Review(
+#     id=1,
+#     product_id=1,
+#     user_id=1,
+#     rating=5,
+#     comment="Very satisfying"
+# )
+# sales1 = Sales(
+#     id=1,
+#     product_id = 1,
+#     user_id = 1,
+#     quantity = 1,
+#     price = 50000
+    
+# )
+# users1 = User(
+#     id=1,
+#     name="Arnold"
+# )
+# category1 = Category(
+#     id=1,
+#     name = "Electronics"
+# )
+
+
+# session.add(commodity1)
+# session.add(review1)
+# session.add(sales1)
+# session.add(users1)
+# session.add(category1)
+
+# session.commit()
+
--- main.py
+++ main.py
@@ -1,45 +1,156 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from Ecommerce import Product,Sales,Review,Category,User,session
-from typing import List,Optional
+from Ecommerce import Product, Sales, Review, Category, User, session
+from typing import List, Optional
 
 app = FastAPI()
-class EcommerceSchema(BaseModel):
-  id:int
-  name:str
-  price:int
-  description:str
-  category_id:int
-  stock:int
-  image_url:str
-  reviews:str
-  class Config:
-    orm_mode = True
-class UpdatedEccomerceSchema(BaseModel):
-  id:Optional[int] = None
-  name:Optional[str]  = None
-  price:Optional[int] = None
-  description:Optional[str] = None
-  category_id:Optional[int] = None
-  stock:Optional[int] = None
-  image_url:Optional[str] = None
-  reviews:Optional[str] = None
-  class Config:
-    orm_mode = True
-  
 
-@app.get('/')
-def get_all_products()->List[EcommerceSchema]:
-  ecommerces = session.query(Product).all()
-  return ecommerces
-@app.get('/products{id}')
-def get_single_product(id:int) ->EcommerceSchema:
-  singleproduct = session.query(Product).filter_by(id=id).first()
-  return singleproduct
-@app.post('/add_product')
-def add_product(product:EcommerceSchema) ->EcommerceSchema:
-  goods = Product(**dict(product))
-  session.add(goods)
+
+class ReviewSchema(BaseModel):
+    id: int
+    product_id: int
+    user_id: int
+    rating: int
+    comment: str
+
+    class Config:
+        orm_mode = True
+
+
+class ProductSchema(BaseModel):
+    id: int
+    name: str
+    price: int
+    description: str
+    category_id: int
+    stock: int
+    image_url: str
+    reviews: List[ReviewSchema]
+
+class ProductPostSchema(BaseModel):
+    id: int
+    name: str
+    price: int
+    description: str
+    category_id: int
+    stock: int
+    image_url: str
+
+    class Config:
+        orm_mode = True
+               
+        
+
+
+class SalesSchema(BaseModel):
+    id: int
+    product_id: int
+    user_id: int
+    quantity: int
+    price: int
+
+    class Config:
+        orm_mode = True
+
+
+class UsersSchema(BaseModel):
+    id: int
+    name: str
+
+    class Config:
+        orm_mode = True
+
+
+class CategorySchema(BaseModel):
+    id: int
+    name: str
+
+    class Config:
+        orm_mode = True
+
+
+class UpdatedProductSchema(BaseModel):
+    id: Optional[int] = None
+    name: Optional[str] = None
+    price: Optional[int] = None
+    description: Optional[str] = None
+    category_id: Optional[int] = None
+    stock: Optional[int] = None
+    image_url: Optional[str] = None
+    reviews: Optional[str] = None
+
+    class Config:
+        orm_mode = True
+
+
+@app.get('/products')
+def get_all_products() -> List[ProductSchema]:
+    products = session.query(Product).all()
+    return products
+
+
+@app.get('/users')
+def get_all_users() -> List[UsersSchema]:
+    users = session.query(User).all()
+    return users
+
+
+@app.get('/categories')
+def get_all_categories() -> List[CategorySchema]:
+    categories = session.query(Category).all()
+    return categories
+
+
+@app.get('/reviews')
+def get_all_reviews() -> List[ReviewSchema]:
+    reviews = session.query(Review).all()
+    return reviews
+
+
+@app.get('/sales')
+def get_all_sales() -> List[SalesSchema]:
+    sales = session.query(Sales).all()
+    return sales
+
+
+@app.get('/products/{id}')
+def get_single_product(id: int) -> ProductSchema:
+    product = session.query(Product).filter_by(id=id).first()
+    return product
+
+
+@app.get('/users/{id}')
+def get_single_user(id: int) -> UsersSchema:
+    user = session.query(User).filter_by(id=id).first()
+    return user
+
+
+@app.get('/categories/{id}')
+def get_single_category(id: int) -> CategorySchema:
+    category = session.query(Category).filter_by(id=id).first()
+    return category
+
+
+@app.get('/reviews/{id}')
+def get_single_review(id: int) -> ReviewSchema:
+    review = session.query(Review).filter_by(id=id).first()
+    return review
+
+
+@app.get('/sales/{id}')
+def get_single_sales(id: int) -> SalesSchema:
+    sales = session.query(Sales).filter_by(id=id).first()
+    return sales
+
+
+
+
+
+@app.post('/product')
+def add_product(payload : ProductPostSchema):
+  product = Product(**dict(payload))
+  session.add(product)
   session.commit()
-  return goods
+  
+  return product
 
