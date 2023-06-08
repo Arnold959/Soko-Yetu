30d5fe7a117a0cf90104b9aa459b86c5c98363ea
winniegithubrit <winnie.jomo@student.moringaschool.com>
2023-06-08 07:42:32 +0300

    added CORS

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from Ecommerce import Product, Sales, Review, Category, User, session
 from typing import List, Optional
 
 app = FastAPI()
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://127.0.0.1:8000"
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class ReviewSchema(BaseModel):
